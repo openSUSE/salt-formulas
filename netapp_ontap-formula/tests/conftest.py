77b648b9d8efbd9d63885a60468aa5083076c239
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2023-06-29 20:52:52 +0200

    netapp_ontap: add LUN mapping test coverage
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

--- netapp_ontap-formula/tests/conftest.py
+++ netapp_ontap-formula/tests/conftest.py
@@ -6,12 +6,13 @@ def target():
     # FIXME make this configurable
     return '10.168.0.97'
 
+
 @pytest.fixture
 def lun(host, target):
     r = host.salt('ontap.provision_lun', ['testlun', '10MB', 'myvol', 'mysvm', 'a lonely lun'])
     assert r['result']
     assert r['status'] == 201
-    r_prequery =  api(target, 'storage/luns')
+    r_prequery =  api(target, 'storage/luns', {'fields': 'comment,space.size,svm.name'})
     records_pre = r_prequery['records']
     for entry, record in enumerate(records_pre):
         if record['name'] == '/vol/myvol/testlun':
@@ -22,11 +23,13 @@ def lun(host, target):
 
     yield myrecord
 
-    r_midquery = api(target, 'storage/luns')
+    r_midquery = api(target, 'storage/luns', {'fields': 'status.mapped'})
     records_mid = r_midquery['records']
     for entry, record in enumerate(records_mid):
         if record['name'] == '/vol/myvol/testlun':
-            myuuid = records_mid[entry]['uuid']
+            myuuid = record['uuid']
+            if record['status']['mapped']:
+                r = host.salt('ontap.unmap_lun', ['testlun', 'myvol', 'mysvm'])
             # probably better to delete using a direct API call here in case there is a problem with delete_lun_uuid
             r = host.salt('ontap.delete_lun_uuid', myuuid)
             assert r['result']
@@ -35,3 +38,22 @@ def lun(host, target):
     r_finquery = api(target, 'storage/luns')
     records_fin = r_finquery['records']
     assert not any(record['uuid'] == myuuid for record in records_fin), 'Failed to clean up LUN fixture'
+
+
+@pytest.fixture
+def lun_mapped(host, target, lun):
+    myuuid = lun['uuid']
+    mypath = lun['name']
+    myvolume, myname = mypath.split('/')[2:]
+    mysvm = lun['svm']['name']
+    r = host.salt('ontap.map_lun', [myname, '900', myvolume, mysvm, 'myigroup'])
+    assert r['result']
+    assert r['status'] == 201
+    r_prequery =  api(target, 'storage/luns', {'fields': 'comment,space.size,svm.name'})
+    records_pre = r_prequery['records']
+    for entry, record in enumerate(records_pre):
+        if record['name'] == '/vol/myvol/testlun':
+            myrecord = records_pre[entry]
+    assert myrecord, 'Did not find any record matching the created LUN fixture name'
+
+    yield myrecord
--- netapp_ontap-formula/tests/test_20_lun.py
+++ netapp_ontap-formula/tests/test_20_lun.py
@@ -70,3 +70,93 @@ def test_lun_resize_reduce_fail(host, target, lun):
             break
     assert myrecord, 'Did not find any record matching the created LUN name'
     assert myrecord['space']['size'] == 10485760
+
+
+@pytest.mark.parametrize("human", [True, False])
+def test_get_lun(host, target, lun, human):
+    mycomment = lun['comment']
+    myuuid = lun['uuid']
+    myname = lun['name']
+    r = host.salt('ontap.get_lun', ['a lonely lun', f'human={human}'])
+    r0 = r[0]
+    assert len(r0.keys()) == 5
+    assert r0['comment'] == mycomment
+    assert r0['name'] == myname
+    if human:
+        expsize = '10MB'
+    elif not human:
+        expsize = 10485760
+    assert r0['space']['size'] == expsize
+    assert r0['status']['mapped'] is False
+    assert r0['uuid'] == myuuid
+
+
+def test_map_lun(host, target, lun):
+    myuuid = lun['uuid']
+    mypath = lun['name']
+    myvolume, myname = mypath.split('/')[2:]
+    mysvm = lun['svm']['name']
+    r = host.salt('ontap.map_lun', [myname, '900', myvolume, mysvm, 'myigroup'])
+    assert r['result']
+    assert r['status'] == 201
+    result_rest = api(target, 'protocols/san/lun-maps', {'lun.uuid': myuuid})
+    records = result_rest['records']
+    r0 = records[0]
+    assert r0['lun']['name'] == mypath
+
+
+def test_get_lun_mapped(host, target, lun_mapped):
+    lun = lun_mapped
+    mypath = lun['name']
+    mycomment = lun['comment']
+    r = host.salt('ontap.get_lun_mapped', mycomment)
+    assert r[mypath] is True
+
+
+def test_get_lun_mapping(host, target, lun_mapped):
+    lun = lun_mapped
+    mysvm = lun['svm']['name']
+    mypath = lun['name']
+    myvolume, myname = mypath.split('/')[2:]
+    r = host.salt('ontap.get_lun_mapping', [myname, myvolume, 'myigroup'])
+    assert r['lun']['name'] == mypath
+    assert r['svm']['name'] == mysvm
+
+
+@pytest.mark.parametrize("igroup", [None, 'myigroup'])
+def test_get_lun_mappings(host, target, lun_mapped, igroup):
+    lun = lun_mapped
+    mysvm = lun['svm']['name']
+    mypath = lun['name']
+    myvolume, myname = mypath.split('/')[2:]
+    params = [myname, myvolume]
+    if igroup is not None:
+        params.append(igroup)
+    r = host.salt('ontap.get_lun_mappings', params)
+    assert len(r) == 1
+    r0 = r[0]
+    if igroup is not None:
+        assert r0['igroup']['name'] == igroup
+    assert r0['lun']['name'] == mypath
+    assert r0['svm']['name'] == mysvm
+
+
+def test_unmap_lun(host, target, lun_mapped):
+    lun = lun_mapped
+    mypath = lun['name']
+    myvolume, myname = mypath.split('/')[2:]
+    r = host.salt('ontap.unmap_lun', [myname, myvolume, 'myigroup'])
+    assert r['result']
+    assert r['status'] == 200
+
+
+# to-do: test multiple unmappings
+def test_unmap_luns(host, target, lun_mapped):
+    lun = lun_mapped
+    mypath = lun['name']
+    myvolume, myname = mypath.split('/')[2:]
+    r = host.salt('ontap.unmap_luns', [myname, myvolume, 'myigroup'])
+    assert len(r) == 1
+    r0 = r[0]
+    assert r0['result']
+    assert r0['status'] == 200
