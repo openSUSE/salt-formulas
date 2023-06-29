190e90fc4d422a6ccccfd493e5442cc66ef4d2a0
Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>
2023-06-29 19:14:53 +0200

    netapp_ontap: expand tests
    - use shared LUN fixture
    - add coverage for deletion and update operations
    
    Signed-off-by: Georg Pfuetzenreuter <georg.pfuetzenreuter@suse.com>

--- netapp_ontap-formula/tests/conftest.py
+++ netapp_ontap-formula/tests/conftest.py
@@ -1,6 +1,37 @@
+from lib import api
 import pytest
 
 @pytest.fixture
 def target():
     # FIXME make this configurable
     return '10.168.0.97'
+
+@pytest.fixture
+def lun(host, target):
+    r = host.salt('ontap.provision_lun', ['testlun', '10MB', 'myvol', 'mysvm', 'a lonely lun'])
+    assert r['result']
+    assert r['status'] == 201
+    r_prequery =  api(target, 'storage/luns')
+    records_pre = r_prequery['records']
+    for entry, record in enumerate(records_pre):
+        if record['name'] == '/vol/myvol/testlun':
+            myrecord = records_pre[entry]
+    assert myrecord, 'Did not find any record matching the created LUN fixture name'
+    myuuid = myrecord['uuid']
+    assert any(record['uuid'] == myuuid for record in records_pre), 'LUN fixture does not show in records'
+
+    yield myrecord
+
+    r_midquery = api(target, 'storage/luns')
+    records_mid = r_midquery['records']
+    for entry, record in enumerate(records_mid):
+        if record['name'] == '/vol/myvol/testlun':
+            myuuid = records_mid[entry]['uuid']
+            # probably better to delete using a direct API call here in case there is a problem with delete_lun_uuid
+            r = host.salt('ontap.delete_lun_uuid', myuuid)
+            assert r['result']
+            assert r['status'] == 200
+
+    r_finquery = api(target, 'storage/luns')
+    records_fin = r_finquery['records']
+    assert not any(record['uuid'] == myuuid for record in records_fin), 'Failed to clean up LUN fixture'
--- netapp_ontap-formula/tests/test_20_lun.py
+++ netapp_ontap-formula/tests/test_20_lun.py
@@ -1,16 +1,72 @@
 from lib import api
+import json
+import pytest
 
+
+@pytest.mark.dependency(name='provision')
 def test_lun_provision(host, target):
-    r = host.salt('ontap.provision_lun', ['testlun', '10MB', 'myvol', 'mysvm', 'a lonely lun'])
+    r = host.salt('ontap.provision_lun', ['provisiontestlun', '10MB', 'myvol', 'mysvm', 'a provisioned lun'])
     assert r['result']
     assert r['status'] == 201
     result_rest = api(target, 'storage/luns', {'fields': 'comment,space.size'})
+    records = result_rest['records']
+    for entry, record in enumerate(records):
+        if record['name'] == '/vol/myvol/provisiontestlun':
+            myrecord = records[entry]
+            break
+    assert myrecord, 'Did not find any record matching the created LUN name'
+    assert myrecord['name'] == '/vol/myvol/provisiontestlun'
+    assert myrecord['comment'] == 'a provisioned lun'
+    assert myrecord['space']['size'] == 10485760
+
+
+@pytest.mark.dependency(depends=['provision'])
+def test_lun_delete_name(host, target):
+    r = host.salt('ontap.delete_lun_name', ['provisiontestlun', 'myvol'])
+    assert r['result']
+    assert r['status'] == 200
+    result_rest = api(target, 'storage/luns', {'fields': 'comment,space.size'})
+    records = result_rest['records']
+    assert not '/vol/myvol/provisiontestlun' in records
+
+
+def test_lun_delete_uuid(host, target, lun):
+    myuuid = lun['uuid']
+    r = host.salt('ontap.delete_lun_uuid', myuuid)
+    assert r['result']
+    assert r['status'] == 200
+    result_rest = api(target, 'storage/luns')
+    records = result_rest['records']
+    assert not any(record['uuid'] == myuuid for record in records)
+
+
+def test_lun_resize(host, target, lun):
+    myuuid = lun['uuid']
+    r = host.salt('ontap.update_lun', [myuuid, '20MB'])
+    assert r['result']
+    assert r['status'] == 200
+    result_rest = api(target, 'storage/luns', {'fields': 'space.size'})
     records = result_rest['records']
     for entry, record in enumerate(records):
         if record['name'] == '/vol/myvol/testlun':
             myrecord = records[entry]
             break
     assert myrecord, 'Did not find any record matching the created LUN name'
-    assert myrecord['name'] == '/vol/myvol/testlun'
-    assert myrecord['comment'] == 'a lonely lun'
+    assert myrecord['space']['size'] == 20971520
+
+
+def test_lun_resize_reduce_fail(host, target, lun):
+    myuuid = lun['uuid']
+    r = host.run_expect([1], f'salt-call --out=json ontap.update_lun {myuuid} 5MB')
+    rout = json.loads(r.stdout)['local']
+    assert not rout['result']
+    assert rout['status'] == 400
+    assert rout['message'] == 'Reducing the size of a LUN might cause permanent data loss or corruption and is not supported by the LUN REST API. The ONTAP command "lun resize" can be used to reduce the LUN size.'
+    result_rest = api(target, 'storage/luns', {'fields': 'space.size'})
+    records = result_rest['records']
+    for entry, record in enumerate(records):
+        if record['name'] == '/vol/myvol/testlun':
+            myrecord = records[entry]
+            break
+    assert myrecord, 'Did not find any record matching the created LUN name'
     assert myrecord['space']['size'] == 10485760
