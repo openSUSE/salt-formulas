a27095875f26485ef5d7592bc7c8fc08f3866c3e
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2024-04-21 04:02:20 +0200

    sysconfig: add combined state
    This introduces a state function which combines the
    suse_sysconfig.header and file.keyvalue functions, which are
    commonly used together to manage both the header and the
    configuration values in sysconfig files.
    
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- sysconfig-formula/_states/suse_sysconfig.py
+++ sysconfig-formula/_states/suse_sysconfig.py
@@ -35,3 +35,73 @@ def header(name, fillup=None, header_pillar=None):
     pattern=patterns['search'],
     repl=patterns['replace'],
   )
+
+def sysconfig(name, key_values, fillup=None, header_pillar=None, upper=True, append_if_not_found=False):
+  """
+  Manages both the header and the key/value pairs in a sysconfig file.
+  name = sysconfig file to manage (relative paths will be appended to /etc/sysconfig/)
+  upper = whether keys should be converted to upper case
+  All other arguments are passed to the equally named arguments in the suse_sysconfig.header and file.keyvalue functions.
+  """
+  if not name.startswith('/'):
+    name = f'/etc/sysconfig/{name}'
+  if upper:
+    key_values = {key.upper(): value for key, value in key_values.items()}
+  returns = {
+    'header': __states__['suse_sysconfig.header'](
+                name=name,
+                fillup=fillup,
+                header_pillar=header_pillar,
+              ),
+    'config': __states__['file.keyvalue'](
+                name=name,
+                append_if_not_found=append_if_not_found,
+                ignore_if_missing=__opts__['test'],
+                key_values=key_values,
+              ),
+  }
+
+  return_keys = returns.keys()
+
+  results = tuple(
+    returns[r].get('result') for r in return_keys
+  )
+  if False in results:
+    result = False
+  elif None in results:
+    result = None
+  elif results[0] and results[1]:
+    result = True
+  else:
+    __salt__['log.error']('suse_sysconfig: result merging failed')
+    result = False
+
+  comments = [
+    returns[r].get('comment') for r in return_keys
+  ]
+  if comments[0] == 'Changes would have been made' or 'is set to be changed' in comments[1]:
+    comment = f'File {name} would be modified'
+  elif comments[0] == 'No changes needed to be made' and not comments[1]:
+    comment = comments[0]
+  elif comments[0] == 'Changes were made' or 'Changed' in comments[1]:
+    comment = f'File {name} modified'
+  else:
+    comment = ' - '.join(comments)
+
+  diffs = [
+    returns[r].get('changes', {}).get('diff') for r in return_keys
+  ]
+
+  ret = {
+    'name': name,
+    'changes': {},
+    'result': result,
+    'comment': comment,
+  }
+
+  if diffs[0] is not None:
+    ret['changes'].update({'diff_header': diffs[0]})
+  if diffs[1] is not None:
+    ret['changes'].update({'diff_config': diffs[1]})
+
+  return ret
