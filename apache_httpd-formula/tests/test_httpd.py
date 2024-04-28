dd9d3fb2c20ba85b1c5ee7d2f0a28cd64fb4d463
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2024-04-28 20:46:11 +0200

    apache_httpd: avoid duplicate options
    Avoid duplicating the three hardcoded options if they are
    additionally specified in the user provided pillar.
    
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- apache_httpd-formula/tests/test_httpd.py
+++ apache_httpd-formula/tests/test_httpd.py
@@ -54,7 +54,7 @@ def test_httpd_config(host, salt_apply, test):
   for file, checksum in {
     'conf.d/remote.conf': '2d4e69a65a3c77743f8504af4ae2415a',
     'vhosts.d/mysite1.conf': '15edeaf0ea295a192e9aa964b011493f',
-    'vhosts.d/mysite2.conf': 'b11ed81a8d1e8f2c5f48a89e9df8c757',
+    'vhosts.d/mysite2.conf': '60c7cf30c4cf87e6ad7cbab904538b73',
     'vhosts.d/status.conf': '41228b88bdb6156773b4e26c39c5d9bc',
   }.items():
     if file.startswith('conf.d'):
